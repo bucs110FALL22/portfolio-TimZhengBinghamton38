2cab80868f49ab05dca4a8483fcce6a726c1b1e7
Tim Zheng <112439904+TimZhengBinghamton38@users.noreply.github.com>
2022-09-07 23:36:57 +0000

    Gave up on for loop for now

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -2,13 +2,31 @@ import turtle
 
 wn = turtle.Screen()
 a_turtle = turtle.Turtle()
-num_sides = int(input("Please input number of sides: "))
 a_turtle.shape("turtle")
 a_turtle.color("purple")
-angle = 360/num_sides
 
-for num_sides in [num_sides]:
-    a_turtle.forward(50)
-    a_turtle.left(angle)
+
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
+
+a_turtle.up()
+a_turtle.color("red")
+a_turtle.forward(125)
+a_turtle.down()
+
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
+a_turtle.forward(50)
+a_turtle.left(90)
 
 wn.exitonclick()
