04a1d55d8ca8217ea3ec769f2198f00a0ccc99ff
Tzheng38 <tzheng38@binghamton.edu>
2022-09-07 16:42:40 -0400

    Less Magic Values and change from float to int

--- ch01/exercises/main.py
+++ ch01/exercises/main.py
@@ -6,8 +6,10 @@ print(myvalues)
 rate = input("Current Exchange Rate of USD to Euros: ")
 amount = input("Amount to Exchange: ")
 
-rate = float(rate)
-amount = float(amount)
+rate = int(rate)
+amount = int(amount)
 total = amount*rate
-result = total - 3.00
+servicefee = 3.00
+result = total - servicefee
+
 print(result)
