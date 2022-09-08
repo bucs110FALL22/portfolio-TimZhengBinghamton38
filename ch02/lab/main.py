c646d783905fb140ad993f29ca7a24191a5e47b6
Tzheng38 <tzheng38@binghamton.edu>
2022-09-08 19:40:49 -0400

    Lab Complete?

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,4 +1,4 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
@@ -6,5 +6,13 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 2
+cost_per_class = cost_per_week/classes_per_week
+print("Cost per class:", cost_per_class)
+
+print(classes_per_week, type(classes_per_week), "and" , cost_per_class, type(cost_per_class))
 
 #Part B
+
+box_of_things = ["steaks", 65 , "a thousand monkeys", 90.87, "hunter2", "NAVY SEAL copypasta", 123456789, "a pine tree appears before you", "the trees speak, and they demand water"]
+print(random.choice(box_of_things))
