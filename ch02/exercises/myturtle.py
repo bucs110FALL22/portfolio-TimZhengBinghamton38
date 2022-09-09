c9c872d6ccdbe81604f327475239770786297259
Tzheng38 <tzheng38@binghamton.edu>
2022-09-09 09:56:53 -0400

    Cleaned code, also figured out for loops (for now)

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -5,28 +5,20 @@ a_turtle = turtle.Turtle()
 a_turtle.shape("turtle")
 a_turtle.color("purple")
 
+square_sides = 4
+# Draws a square
+for i in range(square_sides):
+    a_turtle.forward(50)
+    a_turtle.left(90)
 
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
-
+# Lifts pen up to change the pen color, moves 125 pxiels(?) then places pen down and starts to draw another square
 a_turtle.up()
 a_turtle.color("red")
 a_turtle.forward(125)
 a_turtle.down()
 
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
-a_turtle.forward(50)
-a_turtle.left(90)
+for i in range(square_sides):
+    a_turtle.forward(50)
+    a_turtle.left(90)
 
 wn.exitonclick()
