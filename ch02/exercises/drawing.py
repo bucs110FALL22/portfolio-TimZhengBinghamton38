cafc832cacd7e522a1c570c3fdb3baa89360986d
Tim Zheng <112439904+TimZhengBinghamton38@users.noreply.github.com>
2022-09-12 21:07:58 +0000

    Less Magic Values

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,12 +1,12 @@
 import turtle
 
 window = turtle.Screen()
-
+CIRCLE_DEGREES = 360
 superturtlev2 = turtle.Turtle()
 superturtlev2.color("red")
 
 num_sides = int(input("Enter the number of sides: "))
-angle = 360/num_sides
+angle = CIRCLE_DEGREES / num_sides
 line_length = int(input("Enter the length of the sides: "))
 
 for i in range(num_sides):
