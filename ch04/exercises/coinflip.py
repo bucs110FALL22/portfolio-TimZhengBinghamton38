fc6f515831d0a0dbc5231bce4d2a0621f6b80f9f
Tim Zheng <112439904+TimZhengBinghamton38@users.noreply.github.com>
2022-10-03 21:59:50 +0000

    The turtle has run off, and I can't seem to find it (Turtle moves now)

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,7 +1,19 @@
 import random
 import turtle
 
+
+screenturtle = turtle.Screen()
+
 cointurtle = turtle.Turtle()
+cointurtle.shape("turtle")
 
-
-
+while True:
+  coin = random.randrange(0,2)
+  if coin == 0:
+    cointurtle.left(90)
+    cointurtle.forward(50)
+  elif coin == 1:
+    cointurtle.right(90)
+    cointurtle.forward(50)
+  print(cointurtle.pos())
+screenturtle.exitonclick()
