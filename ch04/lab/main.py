a420d7ae7f0493d05dac389c0076f104e61aa85a
Tzheng38 <tzheng38@binghamton.edu>
2022-10-07 14:34:33 -0400

    Finished lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -1,17 +1,32 @@
+from sys import platlibdir
 import pygame
 import random
 import math
 
-
 pygame.init()
 
-
-
-
-#the actual dart game
+#drawing the board
 dartboard = pygame.display.set_mode(size=(512, 512))
 x , y = (dartboard.get_size())
 # x and y store the screen width and height
+selection_box_blue = pygame.Rect(0,0, (x/2), y)
+selection_box_red = pygame.Rect((x/2),0, x, y)
+pygame.draw.rect(dartboard, [255,0,0], selection_box_red)
+pygame.draw.rect(dartboard, [0,0,255], selection_box_blue)
+pygame.display.flip()
+
+player_bet = 0
+print("Place your bet on which color will win by clicking the color")
+while player_bet == 0:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousecoordinate = pygame.mouse.get_pos()
+            if selection_box_blue.collidepoint(mousecoordinate) == True:
+                player_bet += 1
+                print("You bet on blue winning - best hope they win!")
+            elif selection_box_red.collidepoint(mousecoordinate) == True:
+                player_bet += 2
+                print("You bet on red winning - best hope they win!")
 
 #making backboard
 #defining the bounds of the dartboard - (0,0) is the top left, will never change
@@ -66,4 +81,22 @@ for i in range(initturn, turnamount):
             pygame.draw.circle(dartboard, [0,0,200], rand_dart_pos, 8)
             pygame.display.flip()
             pygame.time.wait(1000)
-print("Red points:",red_player_point,"Blue points:", blue_player_point)
+print("Red points:",red_player_point,"| Blue points:", blue_player_point)
+game_result = 0
+
+if red_player_point == blue_player_point:
+    print("The game ended in a tie")
+elif red_player_point > blue_player_point:
+    game_result += 2
+    print("Red won the game")
+    if game_result == player_bet:
+        print("You have predicted correctly")
+    elif game_result != player_bet:
+        print("You have predicted incorrectly")
+elif blue_player_point > red_player_point:
+    game_result += 1
+    print("Blue won the game")
+    if game_result == player_bet:
+        print("You have predicted correctly")
+    elif game_result != player_bet:
+        print("You have predicted incorrectly")
