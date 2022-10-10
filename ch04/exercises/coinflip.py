974e7b63159469a66c39af5934ec54ab1624de47
Tzheng38 <tzheng38@binghamton.edu>
2022-10-10 17:09:06 -0400

    Turtle Excercise done

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -6,14 +6,31 @@ screenturtle = turtle.Screen()
 
 cointurtle = turtle.Turtle()
 cointurtle.shape("turtle")
+cointurtle.speed(0)
 
-while True:
+angle = 90
+distance = 50
+
+
+
+def isinscreen():
+  turtlex = cointurtle.xcor()
+  turtley = cointurtle.ycor()
+  screenx_range = screenturtle.window_width()/2
+  screeny_range = screenturtle.window_height()/2
+  if abs(turtlex) > screenx_range or abs(turtley) > screeny_range:
+    return False
+  return True
+
+while isinscreen() is True:
   coin = random.randrange(0,2)
   if coin == 0:
-    cointurtle.left(90)
-    cointurtle.forward(50)
+    cointurtle.left(angle)
+    cointurtle.forward(distance)
   elif coin == 1:
-    cointurtle.right(90)
-    cointurtle.forward(50)
+    cointurtle.right(angle)
+    cointurtle.forward(distance)
   print(cointurtle.pos())
-screenturtle.exitonclick()
+else:
+  exit()
+
