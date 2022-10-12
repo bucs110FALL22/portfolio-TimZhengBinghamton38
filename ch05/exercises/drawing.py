3922cb397980ffad854f317d35eaabef5595a6f8
Tzheng38 <tzheng38@binghamton.edu>
2022-10-12 17:40:26 -0400

    attempted exercise

--- ch05/exercises/drawing.py
+++ ch05/exercises/drawing.py
@@ -1,1 +1,15 @@
 import turtle
+
+def drawEqShape(myturtle=None,num_sides = 0, side_length=0):
+    angle = 360/num_sides
+    for i in range(num_sides):
+        myturtle.forward(side_length)
+        myturtle.left(angle)
+
+input_sides = int(input("Enter the number of sides: "))
+input_length = int(input("Enter the length of the sides: "))
+
+
+functionturtle = turtle.Turtle()
+drawEqShape(functionturtle, input_sides, input_length)
+
