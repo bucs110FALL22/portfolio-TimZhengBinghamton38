89090202412879e401bac33e5514203222569889
Tzheng38 <tzheng38@binghamton.edu>
2022-10-17 19:56:16 -0400

    Always returns True - hmmm

--- ch05/exercises/boolean_functions.py
+++ ch05/exercises/boolean_functions.py
@@ -1,31 +1,33 @@
 
 
-
 def percentage_to_letter(score = 0):
-        if score >= 90:
-            lettergrade = str("A")
-        elif 90 > score >= 80:
-            lettergrade = str("B")
-        elif 80 > score >= 70:
-            lettergrade = str("C")
-        elif 70 > score >= 60:
-            lettergrade = str("D")
-        elif 60 > score:
-            lettergrade = str("F")
-        return lettergrade
-#why the heck am i using a list? idk
-def is_passing(grade_convert):
-    passgrade = ["A","B","C"]
-    failgrade = ["D","F"]
-    if grade_to_convert_to_passfail == passgrade[0] or passgrade[1] or passgrade[2]:
-        result = str("Pass")
-    if grade_to_convert_to_passfail == failgrade[0] or passgrade[1]:
-        result = str("Fail")
-    return result
-grade_input = float(input("Input your grade: "))
-        
-grade_to_convert_to_passfail = percentage_to_letter(grade_input)
-final_result = is_passing(grade_to_convert_to_passfail)
+    if score >= 90:
+        lettergrade = "A"
+    elif 90 > score >= 80:
+        lettergrade = "B"
+    elif 80 > score >= 70:
+        lettergrade = "C"
+    elif 70 > score >= 60:
+        lettergrade = "D"
+    elif 60 > score:
+        lettergrade = "F"
+    print(lettergrade)
+    return str(lettergrade)
 
-print(grade_to_convert_to_passfail)
-print(final_result)
+def is_passing(letter = None):
+    if letter == "A" or "B" or "C":
+        final_result = True
+    else:
+        final_result = False
+    print(final_result)
+    return final_result
+
+
+grade_input = float(input("Input your grade: "))
+converted_grade = percentage_to_letter(grade_input)
+print("Letter grade:",converted_grade)
+
+if is_passing(converted_grade) == True:
+    print("Pass")
+elif is_passing(converted_grade) == False:
+    print("Fail")
