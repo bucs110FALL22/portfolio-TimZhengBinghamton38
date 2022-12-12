dca678e5790dba87a0cc7a9da23381c497bc2cc0
Tzheng38 <tzheng38@binghamton.edu>
2022-12-12 09:12:47 -0500

    Cleaned up display and added red herring

--- ch09/final/Quotetrivianew.py
+++ ch09/final/Quotetrivianew.py
@@ -3,25 +3,38 @@ import fakenameproxy
 import random
 
 class Quotetrivia:
-    def __init__(self) -> None:        
+    def __init__(self) -> None:
+        """
+        Initializes the quote api objects
+        args: self
+        return: None
+        """ 
         self.randquoteapi = quoteproxy.Randquoteapi()
         self.fakeapi = fakenameproxy.Fakenameapi()
     
     def triviagameloop(self):
+        """
+        Controls the actual trivia game loop
+        args: self
+        return: "Fail" string if answer input does not match author
+        or None
+        """
         author, quote = self.randquoteapi.get()
+        author2, _ = self.randquoteapi.get()
         name = self.fakeapi.get()
         name2 = self.fakeapi.get()
-        namelist = [author,name,name2]
+        namelist = [author,name,name2,author2]
         random.shuffle(namelist)
         print(quote,"\n")
         print("Who is the author of this quote? \n------------")
         for string in namelist:
             print(string)
-        answer = input("The author is: ")
-        print("Your input was:",answer,"\n")
-        if answer != author:
-            print(f"Fail! The author was {author} \n")
-            return "Fail"
-        elif answer == author:
+        answer = input("You think the author is: ")
+        if answer == author:
             print(f"Pass! The author is indeed {author}")
             exit()
+        elif answer != author:
+            print(f"Fail! The author was {author} \n")
+            if answer not in namelist:
+                print("That wasn't even a choice \n")
+            return "Fail"
--- ch09/final/README.md
+++ ch09/final/README.md
@@ -2,17 +2,21 @@
 
 ## SHORT DESCRIPTION *(1 or 2 sentences describing your program)*
 
-A small quote trivia game in which a quote is given and the use must answer with the quote's author/whom the quote is attributed to
-Three names are given as hints/decoys (one is the answer), though players may choose to input other names not listed
+A small quote trivia game in which a quote is given and the use must answer with the quote's author/whom the quote is attributed to.
+On a correct guess the game ends, but failure to get the correct author will result in the game iterating again until the
+correct answer is given
 
 ## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
 
-For quotes whose authors have one name (e.g. not in the form firstname, lastname) the other two names do not adjust,
-allowing for easier wins
+Authors whose names are not formatted as *firstname* *lastname* appear more obvious, as the fake names do not adjust to match
 
+Unable to limit answers given to names listed (user an input any string).
+
+If a name has characters not on a standard US keyboard, it is difficult to type.
 ## REFERENCES *(any resources you use outside of class materials)*
 
 Fakerapi and its documentation (<https://fakerapi.it/en>)
-quotable and its documentation (<https://github.com/lukePeavey/quotable>)
+
+Quotable and its documentation (<https://github.com/lukePeavey/quotable>)
 
 ## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
